c9914c908caae748291012c018228ddc1fc49986
sho-ts <rrv1011@icloud.com>
2022-05-11 02:10:48 +0900

    flexgap

--- src/components/FlexGap/index.tsx
+++ src/components/FlexGap/index.tsx
@@ -1,9 +1,27 @@
+import { css } from "@emotion/react";
+
 const FlexGap: React.FC = () => {
-  return <div>test</div>;
+  return (
+    <div css={styles.base}>
+      <div css={styles.col}></div>
+      <div css={styles.col}></div>
+      <div css={styles.col}></div>
+      <div css={styles.col}></div>
+    </div>
+  );
 };
 
 const styles = {
-  
-}
+  base: css`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px 30px;
+  `,
+  col: css`
+    width: calc(33% - 30px);
+    height: 200px;
+    border:1px solid #000;
+  `,
+};
 
 export default FlexGap;
